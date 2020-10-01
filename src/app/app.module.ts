748b417f0558c44d98e6af6d29eeb8d0df473374
Roshen Nair <roshen620@gmail.com>
2020-10-01 22:32:41 +0800

    Generated all required components
    Generated the following components: header, footer, home, projects, contact, project-filter, project-card, contact-card, contact-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectFilterComponent } from './projects/project-filter/project-filter.component';
+import { ProjectCardComponent } from './projects/project-card/project-card.component';
+import { ContactCardComponent } from './contact/contact-card/contact-card.component';
+import { ContactFormComponent } from './contact/contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    ProjectFilterComponent,
+    ProjectCardComponent,
+    ContactCardComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/projects/project-filter/project-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-filter',
+  templateUrl: './project-filter.component.html',
+  styleUrls: ['./project-filter.component.css']
+})
+export class ProjectFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
