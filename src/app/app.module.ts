dfc41dd52b4811d79a93ab827ce1fef0ec10c2f8
Roshen Nair <roshen620@gmail.com>
2020-10-22 18:34:40 +0800

    Added & configured Firebase SDK
    Installed firebase & angularfire packages, added Firebase config to Angular environments variable, setup NgModules for Firebase core & products (Analytics & Firestore)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
+// Modules & providers
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -16,25 +22,28 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { BottomNavbarComponent } from './bottom-navbar/bottom-navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    ProjectsComponent,
-    ContactComponent,
-    ProjectFilterComponent,
-    ProjectCardComponent,
-    ContactCardComponent,
-    ContactFormComponent,
-    NotFoundComponent,
-    BottomNavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		HomeComponent,
+		ProjectsComponent,
+		ContactComponent,
+		ProjectFilterComponent,
+		ProjectCardComponent,
+		ContactCardComponent,
+		ContactFormComponent,
+		NotFoundComponent,
+		BottomNavbarComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFireAnalyticsModule,
+		AngularFirestoreModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebase: {
+		apiKey: "AIzaSyB9a4rxG4K6KBMEuNE-gI5f1dnRJS5UOG8",
+		authDomain: "roshen-nair.firebaseapp.com",
+		databaseURL: "https://roshen-nair.firebaseio.com",
+		projectId: "roshen-nair",
+		storageBucket: "roshen-nair.appspot.com",
+		messagingSenderId: "256790090140",
+		appId: "1:256790090140:web:e734b7be60c6d3bac13859",
+		measurementId: "G-L17B12ZC0L"
+	}
 };
 
 /*
