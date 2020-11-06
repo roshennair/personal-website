f8a32be08f3ac7908b8a02c7d1384a2b65bd3746
Roshen Nair <roshen620@gmail.com>
2020-11-06 09:44:24 +0800

    Preparing app for deployment
    Edited npm build command and configured Angular production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyB9a4rxG4K6KBMEuNE-gI5f1dnRJS5UOG8",
+		authDomain: "roshen-nair.firebaseapp.com",
+		databaseURL: "https://roshen-nair.firebaseio.com",
+		projectId: "roshen-nair",
+		storageBucket: "roshen-nair.appspot.com",
+		messagingSenderId: "256790090140",
+		appId: "1:256790090140:web:e734b7be60c6d3bac13859",
+		measurementId: "G-L17B12ZC0L"
+	}
 };
