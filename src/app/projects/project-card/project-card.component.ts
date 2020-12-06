51cb8d5e1101239789e6cd23b8921518ea9b2fc4
Roshen Nair <roshen620@gmail.com>
2020-12-06 12:06:46 +0800

    Improved ProjectCardComponent status
    Implemented dynamic color for project status

--- src/app/projects/project-card/project-card.component.ts
+++ src/app/projects/project-card/project-card.component.ts
@@ -1,21 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
 	selector: 'app-project-card',
 	templateUrl: './project-card.component.html',
 	styleUrls: ['./project-card.component.css']
 })
-export class ProjectCardComponent implements OnInit {
+export class ProjectCardComponent implements AfterViewInit {
 	@Input() categories: string[];
 	@Input() description: string;
 	@Input() image: string;
 	@Input() name: string;
 	@Input() repository: string;
 	@Input() status: string;
+	@ViewChild('statusEl') statusEl: ElementRef;
 
-	constructor() { }
+	constructor(private renderer: Renderer2) { }
 
-	ngOnInit(): void {
+	ngAfterViewInit(): void {
+		const newClass = this.status === 'Ongoing' ? 'ongoing' : 'completed'
+		this.renderer.addClass(this.statusEl.nativeElement, newClass);
 	}
-
 }
